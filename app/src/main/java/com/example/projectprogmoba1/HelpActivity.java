b5b2774ae667b4c5c2d80a0bb014b5918724ded2
Aryawan Wijaya <nikolaus.wijaya@si.ukdw.ac.id>
2019-09-04 19:06:03 +0700

    Tugas3_constraintLinearLayout

--- app/src/main/java/com/example/projectprogmoba1/HelpActivity.java
+++ app/src/main/java/com/example/projectprogmoba1/HelpActivity.java
@@ -10,11 +10,17 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView myTextView = new TextView(this);
+        setContentView(R.layout.activity_help);// utk load layoutnya
+//        TextView myTextView = new TextView(this);//dia buat object textview baru, tanpa load layout, jadi yang ada di layout gk keluar smua, maka harus
+        // pakai yangg bawah poll 22-23
         Bundle b = getIntent().getExtras();
-        String helpText = b.getString("Help String");
+        String tempText = b.getString("Help String");
 
-        myTextView.setText(helpText);
-        setContentView(myTextView);
+//        myTextView.setText(helpText);
+//        setContentView(myTextView);// klo ini kan utk load object tdi tanpa load layout
+
+        TextView textView = findViewById(R.id.textViewCoba);
+        textView.setText(tempText);
+
     }
 }
--- app/src/main/java/com/example/projectprogmoba1/MainActivity.java
+++ app/src/main/java/com/example/projectprogmoba1/MainActivity.java
@@ -14,35 +14,46 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);//dipanggil pertama kali
+        setContentView(R.layout.activity_main); //1 java hanya bisa pakai 1 xml, tapi 1 xml bisa pnya banyak.. ini dia panggil activity_main.xml
 
-        TextView textView =(TextView)findViewById(R.id.mainActivityTextView);
-        textView.setText(R.string.test_untuk_update_view);
+        TextView textView = findViewById(R.id.mainActivityTextView); //R adl kelas yang digenerate olh layout, jadi masuknya di R smua
 
 
-        Button myBtn = (Button)findViewById(R.id.button1);
+        textView.setText(R.string.test_untuk_update_view); //sama seperti variabel global
+        Button myBtn = findViewById(R.id.button1);
         myBtn.setOnClickListener(myBtnClickListener);
-//        myBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                EditText myEditText = (EditText)findViewById(R.id.editText1);
-//                Log.d("Proteintracker",myEditText.getText().toString());
-//            }
-//        });
-        Button helpBtn =(Button)findViewById(R.id.helpButton);
+
+      /*  myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText myEditText = (EditText)findViewById(R.id.editText1);
+                Log.d("Proteintracker",myEditText.getText().toString());
+            }
+        });*/
+        Button helpBtn =findViewById(R.id.helpButton);
         helpBtn.setOnClickListener(helpButtonListener);
 
-        if (savedInstanceState !=null){
+        Button btnLayout = findViewById(R.id.btnTest);
+        btnLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        if (savedInstanceState !=null){ //fungsinya dia itu ngecek kadang klo di rottae hp itu ngerefresh activity trus apa yng sdh ditulis ilang
+            //makkannya di simpan di state trus di cek disini, jika sdh ada tulisane maa dia akan ngeluarke tulisane
             Log.d("ProteinTracker",savedInstanceState.getString("abc"));
         }
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    protected void onSaveInstanceState(Bundle outState) { //fungsingnya utk simpen nilainya
+        super.onSaveInstanceState(outState); //jadi nilainya disimpan di state, trus dipanggil diatas
 
-        outState.putString("abc","test");
+        outState.putString("abc","test");// parameternya yg pertama keynya, yg kedua apa yang mau disimpan
         super.onSaveInstanceState(outState);
     }
 
@@ -52,15 +63,15 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener myBtnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            EditText myEditText = (EditText)findViewById(R.id.editText1);
+            EditText myEditText = findViewById(R.id.editText1);
             Log.d("Proteintracker",myEditText.getText().toString());
         }
-    };
+    };// sama kaya yang di comment diatas, bebas, tapi mending yang diatas lbh cepet
 
     private  View.OnClickListener helpButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(MainActivity.this,HelpActivity.class);
+            Intent intent = new Intent(MainActivity.this,HelpActivity.class); //utk pindah activity, parameter adl kelas ini, kelas 7an
             Bundle b = new Bundle();
             EditText myEditText = (EditText)findViewById(R.id.editText1);
             b.putString("Help String",myEditText.getText().toString());
